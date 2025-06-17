554f90e206ca4f1f146d1941156275de49933568
franklin barco <franklin@flowrms.com>
2025-06-16 23:06:26 -0400

    Add Angular Material components and implement patient form functionality
    - Updated package.json and package-lock.json to include @angular/cdk and @angular/material dependencies.
    - Enhanced index.html to include Google Fonts and Material Icons.
    - Created PatientFormComponent for patient registration with reactive form validation.
    - Styled patient form using SCSS for better layout and user experience.
    - Implemented PatientListComponent to manage patient data and open the patient form dialog for adding new patients.

--- src/app/patients/patient-list/patient-list.component.ts
+++ src/app/patients/patient-list/patient-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { PatientFormComponent } from '../patient-form/patient-form.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { PatientService } from '../../services/patient.service';
 
 @Component({
   selector: 'app-patient-list',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDialogModule, MatNativeDateModule],
   templateUrl: './patient-list.component.html',
   styleUrls: ['./patient-list.component.scss']
 })
-export class PatientListComponent {}
+export class PatientListComponent implements OnInit {
+  patients: any[] = [];
+
+  constructor(private dialog: MatDialog, private patientService: PatientService) {}
+
+  ngOnInit(): void {
+    this.patientService.getAll().subscribe(result => {
+      console.log(result);
+    });
+  }
+
+  addPatient(): void {
+    const dialogRef = this.dialog.open(PatientFormComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.patients.unshift(result);
+      }
+    });
+  }
+}
